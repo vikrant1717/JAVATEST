b2eb376d826a3a216801ce261f34c95104a53409
vikrant11 <vikrantphadtare9@gmail.com>
2023-11-22 15:48:11 +0530

    Update AvoidUsingPredictableRandomValues.java

--- AvoidUsingPredictableRandomValues.java
+++ AvoidUsingPredictableRandomValues.java
@@ -9,6 +9,8 @@ public class AvoidUsingPredictableRandomValues {
         //Random r = new Random();
         return Long.toHexString(r.nextLong());
 
+        int j;
+
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_PREDICTABLE_RANDOM_VALUES/no-detect
         SecureRandom secureRandom = new SecureRandom();
     
